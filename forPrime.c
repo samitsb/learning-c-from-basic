7064ae04e4d589d97b9c351da3dff99eabdb0f81
samit singh bagchan <samitsbagchan@gmail.com>
2025-12-09 07:51:50 +0545

    new code for prime number

---
+++ forPrime.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int num,i,count=0;
+    printf("enter a number:");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {if (num%i==0){
+            count++;
+
+        }
+    }
+    if(count==2)
+
+        printf("%d is a prime number",num);
+    else
+        printf("%d is not a prime number",num);
+    
+    return 0;
+}
